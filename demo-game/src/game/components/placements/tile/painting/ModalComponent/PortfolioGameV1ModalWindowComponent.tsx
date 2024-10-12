cdc3ee7971bd4a5b65f6a0c0e910124d198fa360
Chris <chrisleebed@gmail.com>
2024-10-12 01:00:14 -0700

    add missing saved file

---
+++ demo-game/src/game/components/placements/tile/painting/ModalComponent/PortfolioGameV1ModalWindowComponent.tsx
@@ -0,0 +1,5 @@
+import { createProjectPaintingModalWindowComponent } from "./ProjectModalWindowTemplate";
+
+const PortfolioGameV1ModalWindowComponent = createProjectPaintingModalWindowComponent("PortfolioGameV1");
+
+export default PortfolioGameV1ModalWindowComponent;
